b2e2bb3e475b064a85e8f58ed23078aba812219f
SocialSisterYi <1440239038@qq.com>
2023-09-01 18:00:32 +0800

    🔨人脸识别特殊页检测

--- cxapi/session.py
+++ cxapi/session.py
@@ -1,10 +1,13 @@
+import re
 import time
+from enum import Enum, auto
 from typing import Callable
 
 import cv2
 import numpy as np
 import requests
 import requests.utils
+from bs4 import BeautifulSoup
 from ddddocr import DdddOcr
 from requests.models import Response
 from requests.sessions import Session
@@ -21,7 +24,19 @@ API_CAPTCHA_IMG = "https://mooc1-api.chaoxing.com/processVerifyPng.ac"
 # 接口-提交验证码并重定向至原请求
 API_CAPTCHA_COMMIT = "https://mooc1-api.chaoxing.com/html/processVerify.ac"
 
+# 接口-提交人脸识别结果
+API_FACE_SUBMIT_INFO = "https://mooc1-api.chaoxing.com/mooc-ans/knowledge/uploadInfo"
+
 ocr = DdddOcr(show_ad=False)
+
+
+class SpecialPageType(Enum):
+    """特殊页类型"""
+
+    NORMAL = auto()  # 正常
+    CAPTCHA = auto()  # 人机验证码
+    FACE = auto()  # 人脸识别
+
 
 def identify_captcha(captcha_img: bytes) -> str:
     """验证码识别实现
@@ -32,32 +47,63 @@ def identify_captcha(captcha_img: bytes) -> str:
     """
     img = np.frombuffer(captcha_img, np.uint8)
     img = cv2.imdecode(img, cv2.IMREAD_GRAYSCALE)
-    _, img = cv2.threshold(img, 190, 255, cv2.THRESH_BINARY)    # 二值化
-    img = cv2.bitwise_not(img)                                  # 反色
+
+    # 二值化
+    _, img = cv2.threshold(img, 190, 255, cv2.THRESH_BINARY)
+
+    # 反色
+    img = cv2.bitwise_not(img)
+
+    # 膨胀
     kernal = np.ones([3, 2], np.uint8)
-    img = cv2.dilate(img, kernal, iterations=1)                 # 膨胀处理
-    _, img_data = cv2.imencode('.png', img)
-    code = ocr.classification(img_data.tostring())              # 交给 OCR 识别
+    img = cv2.dilate(img, kernal, iterations=1)
+
+    # 交给 OCR 识别
+    _, img_data = cv2.imencode(".png", img)
+    code = ocr.classification(img_data.tostring())
     return code
+
+
+def get_special_type(resp: Response) -> SpecialPageType:
+    """识别特殊响应页面
+    Args:
+        resp: 响应对象
+    Returns:
+        SpecialPageType: 特殊页面类型
+    """
+    resp_url = URL(resp.url)
+    if resp_url.path.endswith("/antispiderShowVerify.ac"):
+        return SpecialPageType.CAPTCHA
+    elif resp.headers["Content-Type"].startswith("text/html"):
+        html = BeautifulSoup(resp.text, "lxml")
+        if e := html.select_one("body.grayBg script"):
+            if re.search(
+                r"var url = ServerHost.moocDomain \+ _CP_ \+ \"/knowledge/startface",
+                e.text,
+            ):
+                return SpecialPageType.FACE
+    return SpecialPageType.NORMAL
+
 
 class SessionWraper(Session):
     """requests.Session 的封装
     用于处理风控、序列化 ck 和自动重试
     """
-    logger: Logger                                                      # 日志记录器
-    __cb_resolve_captcha_after: Callable[[int], None] = None            # 验证码识别前回调
-    __cb_resolve_captcha_before: Callable[[bool, str], None] = None     # 验证码识别后回调
-    __captcha_max_retry: int                                            # 验证码最大重试
-    __request_max_retry: int                                            # 连接最大重试
-    __request_retry_cnt: int                                            # 连接重试计数
-    __retry_delay: float                                                # 连接重试间隔
-    
+
+    logger: Logger  # 日志记录器
+    __cb_resolve_captcha_after: Callable[[int], None] = None  # 验证码识别前回调
+    __cb_resolve_captcha_before: Callable[[bool, str], None] = None  # 验证码识别后回调
+    __captcha_max_retry: int  # 验证码最大重试
+    __request_max_retry: int  # 连接最大重试
+    __request_retry_cnt: int  # 连接重试计数
+    __retry_delay: float  # 连接重试间隔
+
     def __init__(
         self,
         captcha_max_retry: int = 6,
         request_max_retry: int = 5,
         retry_delay: float = 5.0,
-        **kwargs
+        **kwargs,
     ) -> None:
         """Constructor
         Args:
@@ -69,19 +115,22 @@ class SessionWraper(Session):
         super().__init__(**kwargs)
         # 默认使用 APP 的 UA, 因为一些接口为 APP 独占
         self.headers.update(
-            {"User-Agent": get_ua("mobile"), "X-Requested-With": "com.chaoxing.mobile"}
+            {
+                "User-Agent": get_ua("mobile"),
+                "X-Requested-With": "com.chaoxing.mobile",
+            }
         )
         self.__captcha_max_retry = captcha_max_retry
         self.__request_max_retry = request_max_retry
         self.__request_retry_cnt = 0
         self.__retry_delay = retry_delay
-    
+
     def __cb_resolve_captcha_after(self, times: int):
         """识别验证码前 默认回调
         Args:
             times: 识别循环次数
         """
-        print(f'正在识别验证码，第 {times} 次...')
+        print(f"正在识别验证码，第 {times} 次...")
 
     def __cb_resolve_captcha_before(self, status: bool, code: str):
         """识别验证码后 默认回调
@@ -90,26 +139,26 @@ class SessionWraper(Session):
             code: 识别的验证码
         """
         if status is True:
-            print(f'验证码识别成功：{code}，提交正确')
+            print(f"验证码识别成功：{code}，提交正确")
         else:
-            print(f'验证码识别成功：{code}，提交错误，10S 后重试')
-    
+            print(f"验证码识别成功：{code}，提交错误，10S 后重试")
+
     def reg_captcha_after(self, cb: Callable[[int], None]):
         """注册验证码识别前回调
         Args:
             cb: 回调函数
         """
         self.__cb_resolve_captcha_after = cb
-    
+
     def reg_captcha_before(self, cb: Callable[[bool, str], None]):
         """注册验证码识别后回调
         Args:
             cb: 回调函数
         """
         self.__cb_resolve_captcha_before = cb
-    
+
     def request(self, *args, **kwargs) -> Response:
-        """"requests.Session.request 的 hook 函数
+        """ "requests.Session.request 的 hook 函数
         Args:
             *args, **kwargs: request 的原始参数
         Returns:
@@ -125,22 +174,31 @@ class SessionWraper(Session):
                 return self.request(*args, **kwargs)
             else:
                 raise
-        else:
-            self.__request_retry_cnt = 0
-            resp_url = URL(resp.url)
-            if resp_url.path.endswith("/antispiderShowVerify.ac"):
+        self.__request_retry_cnt = 0
+        match get_special_type(resp):
+            case SpecialPageType.CAPTCHA:
+                # 验证码
                 self.__handle_anti_spider()
-                resp = self.request(*args, **kwargs)  # 递归重发请求
-            return resp
-    
+                # 递归重发请求
+                resp = self.request(*args, **kwargs)
+                return resp
+            
+            case SpecialPageType.FACE:
+                # 人脸识别
+                self.__handle_face_detection(resp)
+                # TODO: 处理成功后重发
+                
+            case SpecialPageType.NORMAL:
+                # 正常响应
+                return resp
+
     def __handle_anti_spider(self) -> None:
-        """处理风控沿验证码
-        """
+        """处理风控沿验证码"""
         self.logger.info("开始处理验证码")
         for retry_times in range(self.__captcha_max_retry):
             self.logger.info(f"验证码处理第 {retry_times + 1} 次")
             self.__cb_resolve_captcha_after(retry_times + 1)
-            
+
             # 这里过快可能导致 capthca 拉取识别，故延时
             time.sleep(5.0)
             captcha_img = self.__get_captcha_image()
@@ -149,7 +207,7 @@ class SessionWraper(Session):
             captcha_code = identify_captcha(captcha_img)
             self.logger.info(f"验证码已识别 {captcha_code}")
             status = self.__commit_captcha(captcha_code)
-            
+
             self.__cb_resolve_captcha_before(status, captcha_code)
             if status is True:
                 # 识别成功退出 retry
@@ -159,22 +217,19 @@ class SessionWraper(Session):
         else:
             # retry 超限
             raise HandleCaptchaError
-    
+
     def __get_captcha_image(self) -> bytes | None:
         """获取验证码图片
         Returns:
             bytes: 验证码图片数据 png 格式
         """
-        resp = self.get(
-            API_CAPTCHA_IMG, 
-            params={'t': round(time.time() * 1000)}
-        )
-        if not resp.ok or resp.headers['Content-Type'] != 'image/png':
+        resp = self.get(API_CAPTCHA_IMG, params={"t": round(time.time() * 1000)})
+        if not resp.ok or resp.headers["Content-Type"] != "image/png":
             self.logger.warning(f"验证码图片获取失败 {resp.status_code}")
             return None
         self.logger.info(f"验证码图片已获取 (大小 {len(resp.content) / 1024:.2f}KB)")
         return resp.content
-    
+
     def __commit_captcha(self, code: str) -> bool:
         """提交验证码
         Args:
@@ -183,18 +238,54 @@ class SessionWraper(Session):
         resp = self.post(
             API_CAPTCHA_COMMIT,
             data={
-                'app': 0,
-                'ucode': code
+                "app": 0,
+                "ucode": code,
             },
-            allow_redirects=False   # 阻止重定向，以进一步操作
+            allow_redirects=False,  # 阻止重定向，以进一步操作
         )
-        
+
         # HTTP 302 即验证正确，HTTP 202 即验证错误
         if resp.status_code == 302:
             self.logger.info(f"验证码验证成功 {code}")
             return True
         self.logger.warning(f"验证码验证失败 {code}")
         return False
+
+    def __handle_face_detection(self, resp: Response):
+        """处理风控人脸识别
+        Args:
+            resp: 响应对象
+        """
+        self.logger.info("开始处理人脸识别")
+        html = BeautifulSoup(resp.text, "lxml")
+        js_code = html.select_one("body.grayBg script").text
+        face_url = URL(re.search(r"\"/knowledge/startface\?(\S+)\"", js_code).group(0))
+        class_id = face_url.query.get("clazzid")
+        course_id = face_url.query.get("courseid")
+        knowledge_id = face_url.query.get("knowledgeid")
+        # TODO: 上传及提交实现
+        ...
+
+    def __submit_faceinfo(self, class_id, course_id, knowledge_id, object_id):
+        """提交人脸识别信息
+        Args:
+            class_id course_id knowledge_id: 课程信息 id
+            object_id: 人脸上传 id
+        """
+        resp = self.post(
+            API_FACE_SUBMIT_INFO,
+            json={
+                "clazzId": class_id,
+                "courseId": course_id,
+                "knowledgeId": knowledge_id,
+                "uuid": "",
+                "qrcEnc": "",
+                "objectId": object_id,
+            },
+        )
+        json_content = resp.json()
+        # TODO: 解析接口返回数据
+        ...
 
     def ck_load(self, ck: dict[str, str]) -> None:
         """加载 dict 格式的 ck
@@ -209,5 +300,6 @@ class SessionWraper(Session):
             dict[str, str]: 欲导入的 key-value 格式的 Cookie
         """
         return requests.utils.dict_from_cookiejar(self.cookies)
+
 
 __all__ = ["SessionWraper"]
