bc36b72d82eb530e29069c9bfba09cbe707d47d1
SocialSisterYi <1440239038@qq.com>
2023-10-17 10:05:44 +0800

    fix: 更新config说明 #75

--- resolver/searcher/restapi.py
+++ resolver/searcher/restapi.py
@@ -19,14 +19,14 @@ class RestApiSearcher(SearcherBase):
     method: Literal["GET", "POST"]
 
     def __init__(
-            self,
-            url,
-            q_field: str = "question",  # 题目文本字段
-            o_field: str | None = None,  # 选项字段
-            a_field: str = "$.data",  # 答案字段 使用 jsonpath 语法
-            headers: dict | None = None,  # 自定义头部
-            ext_params: dict | None = None,  # 扩展请求字段
-            method: Literal["GET", "POST"] = "POST",  # 请求方式
+        self,
+        url,
+        q_field: str = "question",  # 题目文本字段
+        o_field: str | None = None,  # 选项字段
+        a_field: str = "$.data",  # 答案字段 使用 jsonpath 语法
+        headers: dict | None = None,  # 自定义头部
+        ext_params: dict | None = None,  # 扩展请求字段
+        method: Literal["GET", "POST"] = "POST",  # 请求方式
     ) -> None:
         self.session = requests.Session()
         self.url = url
@@ -47,7 +47,7 @@ class RestApiSearcher(SearcherBase):
         self.question_value = question.value
         params = {self.q_field: self.question_value, **self.ext_params}
         if self.o_field and question.options and isinstance(question.options, dict):
-            params[self.o_field] = '#'.join(question.options.values())
+            params[self.o_field] = "#".join(question.options.values())
         try:
             if self.method == "GET":
                 resp = self.session.get(
@@ -76,14 +76,13 @@ class JsonApiSearcher(SearcherBase):
     url: str
 
     def __init__(
-            self,
-            url,
-            q_field: str = "question",  # 题目文本字段
-            o_field: str | None = None,  # 选项字段
-            a_field: str = "$.data",  # 答案字段 使用 jsonpath 语法
-            headers: dict | None = None,  # 自定义头部
-            ext_params: dict | None = None,  # 扩展请求字段
-
+        self,
+        url,
+        q_field: str = "question",  # 题目文本字段
+        o_field: str | None = None,  # 选项字段
+        a_field: str = "$.data",  # 答案字段 使用 jsonpath 语法
+        headers: dict | None = None,  # 自定义头部
+        ext_params: dict | None = None,  # 扩展请求字段
     ) -> None:
         self.params: dict | None = None
         self.question: str | None = None
@@ -98,13 +97,15 @@ class JsonApiSearcher(SearcherBase):
 
     def invoke(self, question: QuestionModel) -> SearcherResp:
         self.question = question.value
-        self.params = {self.q_field: self.question,
-                       "type": question.type.value,
-                       "id": question.id,
-                       **self.ext_params}
+        self.params = {
+            self.q_field: self.question,
+            "type": question.type.value,
+            "id": question.id,
+            **self.ext_params,
+        }
         if question.options is not None:
             if self.o_field and question.options and isinstance(question.options, dict):
-                self.params[self.o_field] = '#'.join(question.options.values())
+                self.params[self.o_field] = "#".join(question.options.values())
             else:
                 self.params["options"] = question.options
         try:
@@ -139,9 +140,9 @@ class EnncySearcher(RestApiSearcher):
         if "".join(jsonpath.compile("$.data.answer").parse(json_content)) == "很抱歉, 题目搜索不到。":
             return SearcherResp(-404, "搜索失败", self, self.question_value, None)
         if "".join(jsonpath.compile("$.data.answer").parse(json_content)) in (
-                "配置为空或者配置错误，请自行检查或者联系作者查看。",
-                "题库配置的“凭证”被刷新，不要刷新你的凭证！只有当你的题库被别人盗用时才能进行刷新操作，否则会导致题库配置失效，请您前往 https://tk.enncy.cn/ "
-                "登录后到个人中心复制题库配置，并重新在脚本设置中粘贴题库配置。",
+            "配置为空或者配置错误，请自行检查或者联系作者查看。",
+            "题库配置的“凭证”被刷新，不要刷新你的凭证！只有当你的题库被别人盗用时才能进行刷新操作，否则会导致题库配置失效，请您前往 https://tk.enncy.cn/ "
+            "登录后到个人中心复制题库配置，并重新在脚本设置中粘贴题库配置。",
         ):
             return SearcherResp(-403, "Token无效", self, self.question_value, None)
         if result := self.rsp_query.parse(json_content):
@@ -160,7 +161,7 @@ class CxSearcher(RestApiSearcher):
             headers={
                 "Content-Type": "application/x-www-form-urlencoded",
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.35",
-                "Authorization": token
+                "Authorization": token,
             },
             a_field="$.data",
         )
@@ -184,7 +185,7 @@ class TiKuHaiSearcher(JsonApiSearcher):
                 "Host": "api.tikuhai.com",
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.60",
                 "referer": "https://mooc1.chaoxing.com",
-                "Content-Type": "application/json"
+                "Content-Type": "application/json",
             },
             a_field="$.data.answer",
             ext_params={
@@ -195,11 +196,16 @@ class TiKuHaiSearcher(JsonApiSearcher):
     def parse(self, json_content: dict | list) -> SearcherResp:
         if jsonpath.compile("$.code").parse(json_content)[0] != 200:
             if jsonpath.compile("$.msg").parse(json_content):
-                soup = BeautifulSoup(jsonpath.compile("$.msg").parse(json_content)[0], 'lxml')
-                msg = soup.find_all('a')
+                soup = BeautifulSoup(jsonpath.compile("$.msg").parse(json_content)[0], "lxml")
+                msg = soup.find_all("a")
                 if msg:
-                    return SearcherResp(-1, "搜索失败,付费库:" + str(msg[0].text) + "请前往购买" + str(
-                        msg[1].attrs['href']), self, self.question, None)
+                    return SearcherResp(
+                        -1,
+                        "搜索失败,付费库:" + str(msg[0].text) + "请前往购买" + str(msg[1].attrs["href"]),
+                        self,
+                        self.question,
+                        None,
+                    )
             return SearcherResp(-404, "搜索失败", self, self.question, None)
         if result := self.rsp_query.parse(json_content):
             return SearcherResp(0, "ok", self, self.question, result[0][0])
@@ -215,7 +221,7 @@ class MukeSearcher(JsonApiSearcher):
             headers={
                 "Host": "api.muketool.com",
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.60",
-                "Content-Type": "application/json"
+                "Content-Type": "application/json",
             },
             a_field="$.data",
         )
@@ -252,5 +258,12 @@ class LyCk6Searcher(JsonApiSearcher):
         return SearcherResp(-500, "未匹配答案字段", self, self.question, None)
 
 
-__all__ = ["RestApiSearcher", "JsonApiSearcher", "EnncySearcher", "CxSearcher", "TiKuHaiSearcher", "MukeSearcher",
-           "LyCk6Searcher"]
+__all__ = [
+    "RestApiSearcher",
+    "JsonApiSearcher",
+    "EnncySearcher",
+    "CxSearcher",
+    "TiKuHaiSearcher",
+    "MukeSearcher",
+    "LyCk6Searcher",
+]
